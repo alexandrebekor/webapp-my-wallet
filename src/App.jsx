6e6ade8cfbe16d7812de5f0de3f40dd900ad9f4c
Alexandre Bekor <contato@alexandrebekor.com>
2022-01-31 19:15:52 -0300

    added usePost hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
+import axios from "axios"
 import useGet from "./useGet"
-
-// axios.get('https://api-my-wallet-default-rtdb.firebaseio.com/Address.json')
-//   .then(response => console.log(response))
-
-// axios.post('https://api-my-wallet-default-rtdb.firebaseio.com/Address.json', {
-//   teste: 'Alexandre'
-// })
+import usePost from "./usePost"
 
 /**
  *  transactions {
@@ -20,17 +15,23 @@ import useGet from "./useGet"
  *  }
  */
 
-const url = import.meta.env.VITE_URL
-
-// const env = import.meta.env.VITE_URL
+const url = `${import.meta.env.VITE_URL}/2022-01/31.json`
 
 function App() {
   const data = useGet(url)
+  const [postData, post] = usePost(url)
+
+  const onSave = () => {
+    post({value: 20, description: 'teste'})
+  }
+
   return (
     <div>
       <h1>My Wallet</h1>
       { data.loading && <h2>Carregando</h2> }
       { JSON.stringify(data) }
+      <button onClick={onSave}>Save</button>
+      {JSON.stringify(postData)}
     </div>
   )
 }
