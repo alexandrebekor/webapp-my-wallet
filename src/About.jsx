ccfba320e1bed11e8ae9f432bb02247d5dc7a294
Alexandre Bekor <contato@alexandrebekor.com>
2022-02-02 20:22:13 -0300

    added react-router-dom

---
+++ src/About.jsx
@@ -0,0 +1,3 @@
+const About = () => <h1>Sobre</h1>
+
+export default About
--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,13 @@
-import { useGet, usePost, useDelete } from "./rest"
+import { Route, Routes } from "react-router-dom"
+import About from "./about"
+import Home from "./Home"
 
-function App() {
-  const results = useGet('report')
-  const [postData, post] = usePost('2022-02')
-  const [destroyData, destroy] = useDelete()
-
-  const handleSave = () => {
-    post({value: 20, description: 'teste'})
-  }
-
-  const handleDelete = () => {
-    destroy('2022-02')
-  }
-
+const App = () => {
   return (
-    <div>
-      <h1>My Wallet</h1>
-      <label>Ano</label>
-      <select>
-        <option value="2021">2021</option>
-        <option value="2022">2022</option>
-        <option value="2023">2023</option>
-      </select>
-      <label>Mês</label>
-      <select>
-        <option value="01">01</option>
-        <option value="02">02</option>
-        <option value="03">03</option>
-        <option value="04">04</option>
-        <option value="05">05</option>
-        <option value="06">06</option>
-        <option value="07">07</option>
-        <option value="08">08</option>
-        <option value="09">09</option>
-        <option value="10">10</option>
-        <option value="11">11</option>
-        <option value="12">12</option>
-      </select>
-      { results.loading && <h2>Carregando</h2> }
-      { !results.loading && (
-        <div>
-          <p>Tabela</p>
-          <table>
-            <thead>
-              <tr>
-                <th>Mês</th>
-                <th>Previsão de Entrada</th>
-                <th>Previsão de Saída</th>
-                <th>Entrada</th>
-                <th>Saída</th>
-              </tr>
-            </thead>
-            <tbody>
-              { Object.keys(results.data).map(item => {
-                return (
-                  <tr key={item}>
-                    <td>{ item }</td>
-                    <td>{ results.data[item].input_prevision }</td>
-                    <td>{ results.data[item].output_prevision }</td>
-                    <td>{ results.data[item].input }</td>
-                    <td>{ results.data[item].output }</td>
-                  </tr>
-                )
-              }) }
-            </tbody>
-          </table>
-        </div>
-      ) }
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Routes>
   )
 }
 
