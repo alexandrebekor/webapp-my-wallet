4f35c2b05fd469818603266cb02c3637b5ba58d4
Alexandre Bekor <contato@alexandrebekor.com>
2022-02-02 19:48:35 -0300

    added endpoint report

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useGet, usePost, useDelete } from "./rest"
 
-const BASE_URL = import.meta.env.VITE_URL
-
 function App() {
-  const data = useGet('2022-02')
+  const results = useGet('report')
   const [postData, post] = usePost('2022-02')
   const [destroyData, destroy] = useDelete()
 
@@ -18,12 +16,57 @@ function App() {
   return (
     <div>
       <h1>My Wallet</h1>
-      { data.loading && <h2>Carregando</h2> }
-      { JSON.stringify(data) }
-      <button onClick={handleSave}>Save</button>
-      { JSON.stringify(postData) }
-      <button onClick={handleDelete}>Delete</button>
-      { JSON.stringify(destroyData) }
+      <label>Ano</label>
+      <select>
+        <option value="2021">2021</option>
+        <option value="2022">2022</option>
+        <option value="2023">2023</option>
+      </select>
+      <label>Mês</label>
+      <select>
+        <option value="01">01</option>
+        <option value="02">02</option>
+        <option value="03">03</option>
+        <option value="04">04</option>
+        <option value="05">05</option>
+        <option value="06">06</option>
+        <option value="07">07</option>
+        <option value="08">08</option>
+        <option value="09">09</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+      </select>
+      { results.loading && <h2>Carregando</h2> }
+      { !results.loading && (
+        <div>
+          <p>Tabela</p>
+          <table>
+            <thead>
+              <tr>
+                <th>Mês</th>
+                <th>Previsão de Entrada</th>
+                <th>Previsão de Saída</th>
+                <th>Entrada</th>
+                <th>Saída</th>
+              </tr>
+            </thead>
+            <tbody>
+              { Object.keys(results.data).map(item => {
+                return (
+                  <tr key={item}>
+                    <td>{ item }</td>
+                    <td>{ results.data[item].input_prevision }</td>
+                    <td>{ results.data[item].output_prevision }</td>
+                    <td>{ results.data[item].input }</td>
+                    <td>{ results.data[item].output }</td>
+                  </tr>
+                )
+              }) }
+            </tbody>
+          </table>
+        </div>
+      ) }
     </div>
   )
 }
