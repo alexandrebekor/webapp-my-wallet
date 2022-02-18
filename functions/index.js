3b56cab241cb6068ab74d75c75e2d2b52f0c957c
Alexandre Bekor <contato@alexandrebekor.com>
2022-02-18 16:34:14 -0300

    added firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,41 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.sum = functions.database
+  .ref("/transactions/{date}")
+  .onWrite(async (change, context) => {
+    const refMonth = admin.database().ref("/report/" + context.params.date);
+    const refTransactions = change.after.ref;
+    const snapShotTransactions = await refTransactions.once("value");
+    const transactions = snapShotTransactions.val();
+
+    let input = 0;
+    let output = 0;
+
+    Object.keys(transactions).forEach((record) => {
+      if (transactions[record].price > 0) {
+        input += parseFloat(transactions[record].price);
+      } else {
+        output += parseFloat(transactions[record].price);
+      }
+    });
+
+    return refMonth.transaction((current) => {
+      if (current === null) {
+        return {
+          input,
+          output,
+          prevision_input: 0,
+          prevision_output: 0,
+        };
+      }
+
+      return {
+        ...current,
+        input,
+        output,
+      };
+    });
+  });
